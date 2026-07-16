f464f0c8a1876f86e0065932e125f78315237a86
nika200-33 <esartianika9@gmail.com>
2026-07-16 15:39:24 +0400

    Add Home component for SportHub Georgia

---
+++ src/srs/src/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1>SportHub Georgia</h1>
+      <p>საიტი წარმატებით აიწყო!</p>
+    </div>
+  );
+}
