5bb3a7c13ee52f5a7dc63c53de59f03d0f451a03
nika200-33 <esartianika9@gmail.com>
2026-07-16 15:45:20 +0400

    Delete src/srs directory

--- src/srs/instrumentation.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Next.js-ის ოფიციალური "instrumentation" hook
- */
-export async function register() {
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    const { validateEnv } = await import("@/lib/env");
-    validateEnv();
-  }
-}
--- src/srs/src/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "40px", textAlign: "center" }}>
-      <h1>SportHub Georgia</h1>
-      <p>საიტი წარმატებით აიწყო!</p>
-    </div>
-  );
-}
