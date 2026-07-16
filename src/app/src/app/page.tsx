2fc9002027dcd7f2f5041ca026546b4416875298
nika200-33 <esartianika9@gmail.com>
2026-07-16 15:48:51 +0400

    Add Home component with initial layout

---
+++ src/app/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", textAlign: "center", fontFamily: "sans-serif" }}>
+      <h1>SportHub Georgia 🚀</h1>
+      <p>საიტი წარმატებით გაეშვა Netlify-ზე!</p>
+    </main>
+  );
+}
